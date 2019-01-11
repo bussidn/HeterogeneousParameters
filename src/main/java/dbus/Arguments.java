0aca5a06bf3bd94527c29c996cd72657fde26cb9
Dorian <johndoe@example.com>
2019-01-11 19:16:47 +0100

    implement other cases

--- src/main/java/dbus/Arguments.java
+++ src/main/java/dbus/Arguments.java
@@ -1,9 +1,31 @@
 package dbus;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 class Arguments {
+
+    private Map<Class<?>, Map<String, Object>> heterogeneousNamedArguments = new HashMap<>();
+
     <T> Optional<T> getArgument(String name, Class<T> clazz) {
-        return Optional.empty();
+        return getOptional(heterogeneousNamedArguments, clazz)
+                .flatMap(m -> getOptional(m, name))
+                .map(clazz::cast);
+    }
+
+    <T> void putArgument(String name, Class<T> clazz, T value) {
+        getOptional(heterogeneousNamedArguments, clazz)
+                .ifPresentOrElse(m -> m.put(name, value), () -> createNewTypeMapWith(clazz, name, value));
+    }
+
+    private <T> void createNewTypeMapWith(Class<T> clazz, String name, T value) {
+        HashMap<String, Object> typeMap = new HashMap<>();
+        typeMap.put(name, value);
+        heterogeneousNamedArguments.put(clazz, typeMap);
+    }
+
+    private <K, V> Optional<V> getOptional(Map<K, V> map, K key) {
+        return Optional.ofNullable(map.get(key));
     }
 }
--- src/test/java/dbus/ArgumentsTest.java
+++ src/test/java/dbus/ArgumentsTest.java
@@ -13,4 +13,48 @@ public class ArgumentsTest {
         Arguments arguments = new Arguments();
         assertThat(arguments.getArgument("test", String.class)).isEqualTo(Optional.empty());
     }
+
+    @Test
+    public void parameters_should_return_the_provided_argument() {
+        // given
+        Arguments arguments = new Arguments();
+        // when
+        arguments.putArgument("test", String.class, "value");
+        // then
+        assertThat(arguments.getArgument("test", String.class)).isEqualTo(Optional.of("value"));
+    }
+
+    @Test
+    public void parameters_should_return_the_provided_the_second_argument() {
+        // given
+        Arguments arguments = new Arguments();
+        String key2 = "string argument 2";
+        // when
+        arguments.putArgument("string argument", String.class, "value");
+        arguments.putArgument(key2, String.class, "value 2");
+        // then
+        assertThat(arguments.getArgument(key2, String.class)).isEqualTo(Optional.of("value 2"));
+    }
+
+    @Test
+    public void parameters_should_accept_different_types() {
+        // given
+        Arguments arguments = new Arguments();
+        // when
+        arguments.putArgument("test", String.class, "value");
+        arguments.putArgument("test 2", Integer.class, 2);
+        // then
+        assertThat(arguments.getArgument("test 2", Integer.class)).isEqualTo(Optional.of(2));
+    }
+
+    @Test
+    public void parameters_should_return_empty_when_asked_with_the_wrong_type() {
+        // given
+        Arguments arguments = new Arguments();
+        String key = "string argument";
+        // when
+        arguments.putArgument(key, String.class, "value");
+        // then
+        assertThat(arguments.getArgument(key, Short.class)).isEqualTo(Optional.empty());
+    }
 }
